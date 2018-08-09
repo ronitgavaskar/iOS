45aba9d244a3c9d736aab3f4984df8b3d9954811
ronitgavaskar <ronitgavaskar@gmail.com>
2018-08-09 11:07:05 -0700

    adding Dummy Login

---
+++ HelloWorld/hello_world_tests/hello_world_tests.swift
@@ -0,0 +1,36 @@
+//
+//  hello_world_tests.swift
+//  hello_world_tests
+//
+//  Created by Precious Metal on 8/8/18.
+//  Copyright © 2018 Ronit Gavaskar. All rights reserved.
+//
+
+import XCTest
+
+class hello_world_tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
